59eb385788a4e1896f29e7338f96b59a0472a54e
Felipe IntelSantos <felipelullio@gmail.com>
2022-05-26 16:16:04 -0300

    [EXPRESS.JS] if receive get request home page
    se receber uma requisição get para a página inicial apresentar uma msg

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-let express = require('express')
+let express = require('express');
+
+let app = express();
+
+// se receber uma requisição get para a página home
+app.get("/", function(req, res){
+   res.send(`
+   <h1 class="header-1">WELCOME TO OUR APP</h1>
+   `);
+});
+
+app.listen(3000);
